66ffde857f7235da3479fcb55547be7ff6931116
SiriJV <siri.viren@icloud.com>
2025-06-18 23:56:35 +0200

    add placeholder styling to profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const ProfilePage = () => {
     return (
         <>
-            <View>
-                <Text>hello this is profile page</Text>
+            <View style={styles.container}>
+                <Text style={styles.text}>profile page is empty at the moment</Text>
             </View>
         </>
     )
 }
 
 export default ProfilePage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: '#121212',
+  },
+  text: {
+    color: "white"
+  },
+});
