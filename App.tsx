ecae45f4f34d5c2aa8ce9792a0c7c499d53faf6a
SiriJV <siri.viren@icloud.com>
2025-06-18 23:55:53 +0200

    cleanup app

--- App.tsx
+++ App.tsx
@@ -1,36 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Game from './src/components/Game';
-import "react-native-gesture-handler";
 import React from 'react';
+import "react-native-gesture-handler";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import SpotifyLogin from './src/SpotifyLogin';
-
-import * as AuthSession from 'expo-auth-session';
 import NavBar from './src/components/NavBar';
 
 
 const App = () => {
-  const redirectUri = AuthSession.makeRedirectUri();
-  console.log("Redirect URI:", redirectUri);
-
-
-  return (
-    <GestureHandlerRootView style={{ flex: 1, backgroundColor: "black"}}>
-      {/* <Game /> */}
-      {/* <SpotifyLogin /> */}
-      <NavBar />
-    </GestureHandlerRootView>
-  );
+    return (
+        <GestureHandlerRootView style={{ flex: 1, backgroundColor: "#121212"}}>
+          <NavBar />
+        </GestureHandlerRootView>
+    );
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: '#fff',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
-});
